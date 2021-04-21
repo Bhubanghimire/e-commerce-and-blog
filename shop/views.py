2a228dbcdcaafb6a3bbd3be26a07aa891b1cb160
Bhuban Ghimire <bhubanghimire3130.com>
2021-04-21 09:17:18 +0545

    frontend show

--- shop/views.py
+++ shop/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Product
+from math import ceil
 
 # Create your views here.
 def home(request):
     all_products = Product.objects.all()
-    param={"product":all_products}
+    print(all_products)
+    n=len(all_products)
+    nslide = n//4 + ceil((n/4)-(n//4))
+    param={"no_of_slide":nslide, 'range':range(1,nslide), "product":all_products}
     return render(request,"shop/home.html",param)
 
 def about(request):
